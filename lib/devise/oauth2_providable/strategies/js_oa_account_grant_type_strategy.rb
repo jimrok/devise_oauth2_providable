4343401f4e669a7f10d5fc22f4f31498ca4edb8e
Jimrok <jiang717@gmail.com>
2013-11-09 23:53:45 +0800

    change the auth check method.

--- lib/devise/oauth2_providable/strategies/js_oa_account_grant_type_strategy.rb
+++ lib/devise/oauth2_providable/strategies/js_oa_account_grant_type_strategy.rb
@@ -9,20 +9,20 @@ module Devise
       end
 
       def authenticate_grant_type(client)
-            username = params[:username]
-            password = params[:password]
-            form_data= {:userid=>username,:password=>password}
-            res = Net::HTTP.post_form(URI.parse('http://emis.js.cmcc/access/sso'), form_data)
-            if( res.code=="200" ) then
-                if(res.body=~ /\/form\/@action='\/access\/sso'/) then
-                    oauth_error! :invalid_grant, '用户名或密码错误'     
-                else
-                    account = Account.where(:email=>username, :actived=>true).first
-                    success! account
-                end
-            else
-                oauth_error! :invalid_grant, 'OA认证服务器异常'      
-            end
+        username = params[:username]
+        password = params[:password]
+        form_data= {:userid=>username,:password=>password}
+        res = Net::HTTP.post_form(URI.parse('http://emis.js.cmcc/access/sso'), form_data)
+
+        if(res['Set-Cookie'] && res['Set-Cookie'].start_with?("ObSSOCookie=")) then
+          account = Account.where(:email=>username, :actived=>true).first
+          success! account
+        elsif res.code[0].in? ["4","5"] then
+          oauth_error! :invalid_grant, 'OA认证服务器异常'
+        else
+          oauth_error! :invalid_grant, '用户名或密码错误'
+        end
+        
       end
     end
   end
