039c284de7e6679dd09c3c3f28012d5dd1f5abc6
vulgarcoder <vulgarcoder@gmail.com>
2013-12-20 14:15:51 +0800

    Change time to local time.

--- lib/devise/oauth2_providable/strategies/oauth2_refresh_token_grant_type_strategy.rb
+++ lib/devise/oauth2_providable/strategies/oauth2_refresh_token_grant_type_strategy.rb
@@ -18,7 +18,7 @@ module Devise
           if(account_id) then
             device=ApnDevice.find_by_account_id_and_deleted(account_id,0)
             unless device.nil?
-              error_message="您的账号已于#{device.updated_at.strftime("%Y-%m-%d %H:%M")}在其它地方登录。登录设备是#{device.device_name}，请注意账号安全。" 
+              error_message="您的账号已于#{device.updated_at.localtime.strftime("%Y-%m-%d %H:%M")}在其它地方登录。登录设备是#{device.device_name}，请注意账号安全。" 
             end
           end
           oauth_error! :invalid_grant, error_message
