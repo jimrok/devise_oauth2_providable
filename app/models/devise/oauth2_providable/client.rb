adb89b82c3acf5a02361c739ae496b2cb8de9843
vulgarcoder <vulgarcoder@gmail.com>
2014-02-27 20:51:53 +0800

    Added ad_hoc.

--- app/models/devise/oauth2_providable/client.rb
+++ app/models/devise/oauth2_providable/client.rb
@@ -10,11 +10,11 @@ class Devise::Oauth2Providable::Client < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :identifier, :presence => true, :uniqueness => true
 
-  attr_accessible :name, :identifier, :website, :redirect_uri, :latest_version_id, :description,:upgrade_url
+  attr_accessible :name, :identifier, :website, :redirect_uri, :latest_version_id, :description,:upgrade_url,:at_hoc
   belongs_to :latest_version, :class_name => "Oauth2ClientVersion", :foreign_key => "latest_version_id"
 
   def file_name
-      name+".apk"
+      name
   end
   def file_dir
     File.realpath("#{Rails.root}/contents")+"/applications/#{id}/"
--- lib/devise/oauth2_providable/version.rb
+++ lib/devise/oauth2_providable/version.rb
@@ -1,5 +1,5 @@
 module Devise
   module Oauth2Providable
-    VERSION = "1.1.10"
+    VERSION = "1.1.11"
   end
 end
