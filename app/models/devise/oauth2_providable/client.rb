7f585f2f4d217fa465dd053b4a75ab803655a084
vulgarcoder <vulgarcoder@gmail.com>
2014-03-20 17:26:04 +0800

    added at_hoc

--- app/models/devise/oauth2_providable/client.rb
+++ app/models/devise/oauth2_providable/client.rb
@@ -10,7 +10,6 @@ class Devise::Oauth2Providable::Client < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :identifier, :presence => true, :uniqueness => true
 
-  attr_accessible :name, :identifier, :website, :redirect_uri, :latest_version_id, :description,:upgrade_url,:at_hoc
   belongs_to :latest_version, :class_name => "Oauth2ClientVersion", :foreign_key => "latest_version_id"
 
   def file_name
