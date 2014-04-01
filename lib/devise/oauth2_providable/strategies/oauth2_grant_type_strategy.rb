446e86130b296a010467404fc6a1c339d73652c8
vulgarcoder <vulgarcoder@gmail.com>
2014-04-01 16:28:38 +0800

    Bump version to 1.1.21

--- lib/devise/oauth2_providable/strategies/oauth2_grant_type_strategy.rb
+++ lib/devise/oauth2_providable/strategies/oauth2_grant_type_strategy.rb
@@ -19,8 +19,8 @@ module Devise
 
       def authenticate!
         app_id = params[:app_id] || params[:client_id]
-        app_key = params[:app_key] || params[:client_secret]
-        client_id, client_secret = request.authorization ? decode_credentials : [app_id, app_key]
+        app_secret = params[:app_secret] ||params[:app_key] || params[:client_secret]
+        client_id, client_secret = request.authorization ? decode_credentials : [app_id, app_secret]
         client = Devise::Oauth2Providable::Client.find_by_identifier client_id
         if client && client.secret == client_secret
           env[Devise::Oauth2Providable::CLIENT_ENV_REF] = client
